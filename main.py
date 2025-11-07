953b0c24efd9c71949c161c790031d9c97a42a57
Tainã Sichero Dulcetti <mmbjjs@gmail.com>
2025-11-07 17:30:28 -0300

    Add email configuration settings in main.py
    adicionando variáveis de configuração do e-mail

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 Código em Python
+# --- Configurações do e-mail ---
+remetente = "SEU_EMAIL@gmail.com"           # quem envia
+senha = "SENHA_DO_APP"                      # senha de app (não sua senha normal!)
+destinatario = "DESTINATARIO@gmail.com"     # quem vai receber
+
+assunto = "Teste de automação de e-mail"
+mensagem = "Olá! Este é um e-mail automático enviado pelo meu projeto Python no GitHub 🚀"
